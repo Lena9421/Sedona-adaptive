03af5295f31567905f405ada0702417d64620118
Elena Krasnikova <elena.krasnikova.94@mail.ru>
2017-10-27 00:14:31 +0300

    adaptive layout

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var map;
+function initMap () {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat:34.87, lng: -111.79},
+        zoom: 6
+    });
+    var marker = new google.maps.Marker({
+        position: {lat:34.87, lng: -111.79},
+        map: map
+    });
+}
